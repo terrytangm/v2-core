b73bfe3036a3eb9f50e69b3b4b6c2b9b2a6df0b7
Terry Tang <mtang@realfeng.com>
2023-07-13 00:47:58 +0800

    modified:   hardhat.config.js 	modified:   package.json

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,21 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: {
+    // 可修改编译器版本
+    compilers: [
+      {
+        version: "0.8.18",
+      },
+      {
+        version: "0.6.12",
+      },
+      {
+        version: "0.8.7",
+      },
+      {
+        version: "0.5.16",
+      },
+    ],
+  },
 };
